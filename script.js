018154e97cc406b97d7175685d59b4478f12a0bf
reyno405 <reyno405@gmail.com>
2022-08-22 12:32:49 -0400

    this update includes created elements that display current weather

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// variables to be called gloabally
 var cities = [];
 
 var cityFormEl=document.querySelector("#city-search-form");
@@ -38,3 +39,46 @@ var getCityWeather = function(city){
         });
     });
 };
+
+var displayWeather = function(weather, searchCity){
+    // clear olld content
+    weatherContainerEl.textContent= "";
+    citySearchInputEl.textContent=searchCity;
+
+    // creates date element
+    var currentDate = document.createElement("span");
+    currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
+
+    // create and image element
+    var weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    citySearchInputEl.appendChild(weatherIcon);
+
+    // create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item"
+    //create a span element to hold Humidity data
+   var humidityEl = document.createElement("span");
+   humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+   humidityEl.classList = "list-group-item"
+
+   //create a span element to hold Wind data
+   var windSpeedEl = document.createElement("span");
+   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+   windSpeedEl.classList = "list-group-item"
+
+   //append to container
+   weatherContainerEl.appendChild(temperatureEl);
+
+   //append to container
+   weatherContainerEl.appendChild(humidityEl);
+
+   //append to container
+   weatherContainerEl.appendChild(windSpeedEl);
+   
+   var lat = weather.coord.lat;
+   var lon = weather.coord.lon;
+   getUvIndex(lat,lon)
+}
